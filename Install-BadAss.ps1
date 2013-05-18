3195e5e44bbb68f281f8905998cdb44175151326
Appetite Oven <appetitieovenllc@outlook.com>
2013-05-17 20:54:05 -0700

    g

--- Install-BadAss.ps1
+++ Install-BadAss.ps1
@@ -1,22 +1,26 @@
+
+
+
 function Install-BadAss
 {
     $moduleName = "BadAss"
     $downloadurl = "https://raw.github.com/appetiteoven/$moduleName/master/$($moduleName).psm1"
     	
     $UserModulePath = Join-Path -Path ([Environment]::GetFolderPath('MyDocuments')) -ChildPath WindowsPowerShell\Modules
+	$ModulePath = $UserModulePath + "\$($moduleName)"
     		
-	New-Item ($UserModulePath + "\$($moduleName)\") -ItemType Directory -Force | out-null
+	New-Item $ModulePath -ItemType Directory -Force | out-null
 	
 	Write-Host "Downloading module from `n $($downloadurl)" 
 	
 	$client = (New-Object Net.WebClient)
 	$client.Proxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
 	
-	$savepath = $UserModulePath + "\$($moduleName)\$($moduleName).psm1"
+	$savepath = "$ModulePath\$($moduleName).psm1"
 	
 	$client.DownloadFile($downloadurl, $savepath)
 	
-	Import-Module -Name $UserModulePath\$moduleName -Force 
+	Import-Module -Name $ModulePath -Force 
 	
 	Write-Host "$($moduleName) is installed and ready to use. You bad ass you." -Foreground Green
 }
